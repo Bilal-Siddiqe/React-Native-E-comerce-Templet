cf6d72cf5e48758f607277c10b37c332754b695b
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-03-22 11:14:32 +0500

    Cart Screen added

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -11,20 +11,26 @@ const CartScreen = () => {
             <Text style={{ backgroundColor: "white", color: "black", paddingLeft: 25, paddingTop: 20, fontWeight: "bold", fontSize: 18, paddingBottom: 10 }}>Shopping Cart</Text>
 
 
-            <Flex justifyContent="center" alignItems="center" bg="white" padding="2">
-                <Box borderWidth="1" bg="white" width="320" height="100" borderRadius="10" padding="2">
-                    <Flex justifyContent="center" alignItems="center" bg="white" padding="2">
+            <Flex bg="white" paddingY="2">
+                <Box borderWidth="1" bg="white" width="320" height="100" borderRadius="10" paddingY="2">
+                    <Flex flexDirection="row" bg="white" paddingY="2">
                         <Image source={{ uri: "https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" }}
-                            style={{ width: 100, height: 20, marginHorizontal: 15 }} />
+                            style={{ width: 100, height: 70, marginHorizontal: 15 }} />
+
+                        <Box>
+                            <Text style={{ backgroundColor: "white", color: "black", fontSize: 10 }}>Shopping Cart</Text>
+
+                        </Box>
+
+                        <Flex>
+
+
+
+                        </Flex>
+
                     </Flex>
                 </Box>
             </Flex>
-
-
-
-
-
-
 
             {/* <Footer /> */}
 
