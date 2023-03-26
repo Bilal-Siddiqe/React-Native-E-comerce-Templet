3377b85aa7e02f3f71351a783031c9e389df8d12
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-03-27 03:18:53 +0500

    Home Navigation Done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,11 +6,11 @@ import HomeBody from '../Components/HomeScreen/HomeBody';
 import Footer from '../Components/Footer';
 
 
-const Home = () => {
+const Home = ({ navigation }) => {
     return (
         <NativeBaseProvider >
             <HomNav />
-            <HomeBody />
+            <HomeBody nvg = {() => { navigation.navigate('Categories') }}/>
             <Footer/>
         </NativeBaseProvider >
     )
