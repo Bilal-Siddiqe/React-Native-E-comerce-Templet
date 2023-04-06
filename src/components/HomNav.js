c5783a5d7f1331186cbeabfa7e29e6cb0a07afaf
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-04-07 02:13:56 +0500

    Heart Icon Added to HomNav & Also Made Touchable

--- src/components/HomNav.js
+++ src/components/HomNav.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Image } from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 import { Text, Box, Flex, Input } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome"
 
 const HomNav = () => {
     return (
@@ -10,16 +11,19 @@ const HomNav = () => {
                 <Flex direction="row" justifyContent='space-between' alignItems='center' px="5" >
                     <Image source={{ uri: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }}
                         style={{ width: 35, height: 35, borderRadius: 100 }} />
-                    <Text style={{fontSize:20, fontWeight:"bold", color:"purple"}}>Otrixweb</Text>
-                    <Image source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTbLAedxJInRn8rGFYyxHCJz4bHQaygxDFMw&usqp=CAU' }}
-                        style={{ width: 20, height: 20 }} />
+                    <Text style={{ fontSize: 20, fontWeight: "bold", color: "purple" }}>Otrixweb</Text>
+                    
+                    <TouchableOpacity onPress={()=>{}}>
+                        <Icon name="heart-o" size={20} color={"red"} />
+                    </TouchableOpacity>
+
                 </Flex>
             </Box>
             <Box bg="white" p="2">
                 <Input mx="3" placeholder="Search Products" bgColor={"blueGray.100"} />
             </Box>
         </>
-    )       
+    )
 }
 
 export default HomNav
